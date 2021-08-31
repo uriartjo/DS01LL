e6e821d60115b0ff8bea12a1c78079d693530973
Joey Uriarte <Joey.Uriarte@saintandrews.net>
2021-08-31 09:41:11 -0400

    Initial commit

---
+++ src/com/company/MyLinkedList.java
@@ -0,0 +1,67 @@
+package com.company;
+
+public class MyLinkedList {
+
+
+    public boolean add(Object object) {
+        return false;
+    }
+
+    public void add(int index, Object object) {
+
+    }
+
+    public void addFirst(Object object) {
+
+    }
+
+    public void addLast(Object object) { //Identical to add but with a void return type
+
+    }
+
+    public void clear() { //Empties LL
+
+    }
+
+    public boolean contains(Object object) {
+        return false;
+    }
+
+    public Object getFirst() {
+        return null;
+    }
+
+    public Object getLast() {
+        return null;
+    }
+
+    public int indexOf(Object object) {
+        return 0;
+    }
+
+    public int lastIndexOf(Object object) {
+        return 0;
+    }
+
+    public Object poll() { //Returns head element
+        return null;
+    }
+
+    public Object pollLast() { //Returns tail element
+        return null;
+    }
+
+    public Object remove(Object object) {
+        return null;
+    }
+
+    public Object set(int index, Object object) { //Return value should be item being replaced
+        return null;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+
+}
